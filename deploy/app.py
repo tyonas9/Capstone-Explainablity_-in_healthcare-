4c519b6fba32c8faf8e7fb5bbae0b8a321b254f6
Yonas Tesh <tyonas@gmail.com>
2022-09-23 22:21:27 -0400

    added app.py for WSI image upload
    app.py upload a whole slide image and puts it into the WSI folder

--- deploy/app.py
+++ deploy/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from PIL import ImageFile
-import openslide
+import os
 
 def main():
     st.title("Image upload")
@@ -9,13 +8,17 @@ def main():
     choice = st.sidebar.selectbox("Menu",menu)
 
     if choice == "Image":
-        st.subheader("Image")
-        image_file = st.file_uploader("Upload Image", type=["svs"])
+        st.subheader("Upload Images")
+        image_file = st.file_uploader("Upload an Image", type=["svs"])
         if image_file is not None:
-            # see details
-            st.write(type(image_file))
+            # see image details
+            file_details = {"FileName":image_file.name, "FileType":image_file.type}
 
-
+            #Saving image file in a WSI folder
+            with open(os.path.join("WSI",image_file.name),"wb") as f:
+                f.write(image_file.getbuffer())
+            
+            st.success("WSI image has been uploaded")
 
 if __name__ == '__main__':
 	main()
